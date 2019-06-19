9de1991bbf6bed996864f67c598294839c55165f
Michael D <miketdennison@gmail.com>
2019-06-19 15:49:55 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# TrainScheduler
+# Train Scheduler
+
+Train Schedule pulls information from firebase to dynamically update train times for users.
+
+## Getting Started
+Click the live link below.
+Live Link: N/A
+
+## Built With
+* HTML
+* CSS
+* Bootstrap
+* JavaScript
+* jQuery
+* [Moment.js](https://momentjs.com/) - An open source library used licensed by MIT
+* [Firebase](https://firebase.google.com) - Used for a Realtime Database
+
+## Author(s)
+
+* **Michael Dennison** 
+
---
+++ assets/javascript/index.js
@@ -0,0 +1,17 @@
+// Configuration for database
+var firebaseConfig = {
+    apiKey: "AIzaSyCwQQC3Rw9chSg9umwogDSMPPScycdRbEo",
+    authDomain: "trainscheduler-3818d.firebaseapp.com",
+    databaseURL: "https://trainscheduler-3818d.firebaseio.com",
+    projectId: "trainscheduler-3818d",
+    storageBucket: "trainscheduler-3818d.appspot.com",
+    messagingSenderId: "233016200919",
+    appId: "1:233016200919:web:dfda1992f6b706d2"
+};
+
+// Initialize Firebase database with config
+firebase.initializeApp(firebaseConfig);
+
+// Reference to Firebase database
+var database = firebase.database();
+
