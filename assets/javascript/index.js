862fc17fa7e7a9451ea7f0684c46f325ab0eac02
Michael D <miketdennison@gmail.com>
2019-06-19 19:49:10 -0500

    basic js logic working, need to implement moment js, and validate time data

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -12,5 +12,57 @@ var firebaseConfig = {
 // Initialize Firebase database with config
 firebase.initializeApp(firebaseConfig);
 
-// Reference to Firebase database
+// Reference to Firebase database & Root
 var database = firebase.database();
+var dbRef = database.ref();
+
+// If a child is modified, update the view
+dbRef.on("child_added", snapshot => updateView(snapshot));
+dbRef.on("child_changed", snapshot => updateView(snapshot));
+dbRef.on("child_removed", snapshot => updateView(snapshot));
+
+// When submit button is clicked, update the database
+$("#submit-button").on("click", function () {
+    var empty = false;
+    $(":text").each(function () {
+        if ($(this).val() === "") {
+            empty = true;
+        }
+    });
+    // If all text field are non-empty, verify frequency data and time data
+    if (!empty) {
+        var frequencyTime = $("#add-frequency-time").val();
+        if (!isNaN(parseInt(frequencyTime))) {
+            updateDb();
+        }
+    }
+});
+
+// Trigged via listners for child modifications
+function updateView(snapshot) {
+    var sVal = snapshot.val();
+    var row =
+        "<tr>" +
+        "<td>" + sVal.trainName + "</td>" +
+        "<td>" + sVal.trainDst + "</td>" +
+        "<td>" + sVal.trainFreq + "</td>" +
+        "<td>" + sVal.firstTrain + "</td>" +
+        "<td>" + sVal.minutesAway + "</td>" +
+        "</tr>";
+    $("#train-data").append(row);
+
+    $("#add-train-name").text('');
+    $("#add-destination-name").text('');
+    $("#add-first-train-time").text('');
+    $("#add-frequency-time").text('');
+}
+
+// Triggered via submit-button listener
+function updateDb() {
+    dbRef.push({
+        trainName: $("#add-train-name").val(),
+        trainDst: $("#add-destination-name").val(),
+        firstTrain: $("#add-frequency-time").val(),
+        trainFreq: $("#add-first-train-time").val(),
+    });
+}
