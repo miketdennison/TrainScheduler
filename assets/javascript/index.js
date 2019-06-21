df0fa3aaa8bde9b94de471c5160dc4bf855b98ba
Michael D <miketdennison@gmail.com>
2019-06-20 19:08:52 -0500

    javascript functioning, need to add clock

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -18,12 +18,14 @@ var dbRef = database.ref();
 
 // If a child is modified, update the view
 dbRef.on("child_added", snapshot => updateView(snapshot));
-dbRef.on("child_changed", snapshot => updateView(snapshot));
-dbRef.on("child_removed", snapshot => updateView(snapshot));
 
 // When submit button is clicked, update the database
 $("#submit-button").on("click", function () {
+    // Prevent page reload
+    event.preventDefault();
+    // Variable used to discover empty text-fields
     var empty = false;
+    // Check whether any text-fields are empty
     $(":text").each(function () {
         if ($(this).val() === "") {
             empty = true;
@@ -41,28 +43,43 @@ $("#submit-button").on("click", function () {
 // Trigged via listners for child modifications
 function updateView(snapshot) {
     var sVal = snapshot.val();
+
+    // Values used for calculating next train's arrival time
+    var tFrequency = sVal.trainFreq;
+    var firstTime = sVal.firstTrain;
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    var tRemainder = diffTime % tFrequency;
+    var tMinutesTillTrain = tFrequency - tRemainder;
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    var arrivalTime = moment(nextTrain).format("hh:mm");
+
+    // Update Rows
     var row =
         "<tr>" +
         "<td>" + sVal.trainName + "</td>" +
         "<td>" + sVal.trainDst + "</td>" +
         "<td>" + sVal.trainFreq + "</td>" +
-        "<td>" + sVal.firstTrain + "</td>" +
-        "<td>" + sVal.minutesAway + "</td>" +
+        "<td>" + arrivalTime + "</td>" + // first train arrival
+        "<td>" + tMinutesTillTrain + "</td>" + // Minutes Away
         "</tr>";
     $("#train-data").append(row);
 
-    $("#add-train-name").text('');
-    $("#add-destination-name").text('');
-    $("#add-first-train-time").text('');
-    $("#add-frequency-time").text('');
+    // Empty text fields
+    $("#add-train-name").val("");
+    $("#add-destination-name").val("");
+    $("#add-first-train-time").val("");
+    $("#add-frequency-time").val("");
 }
 
 // Triggered via submit-button listener
 function updateDb() {
     dbRef.push({
-        trainName: $("#add-train-name").val(),
-        trainDst: $("#add-destination-name").val(),
-        firstTrain: $("#add-frequency-time").val(),
-        trainFreq: $("#add-first-train-time").val(),
+        trainName: $("#add-train-name").val().trim(),
+        trainDst: $("#add-destination-name").val().trim(),
+        trainFreq: $("#add-frequency-time").val().trim(), 
+        firstTrain: $("#add-first-train-time").val().trim(),
     });
 }
